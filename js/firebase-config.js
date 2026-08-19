fc09a8b3c943b5db5c6722030aea7fe6ef8ee076
Lance Jenner <lance.jenner@outlook.com>
2026-08-19 15:20:50 +1200

    Implement Firebase app scaffold

---
+++ js/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBDkcetiH0ens8i5sWfpZqWK7hcJiu56UQ",
+  authDomain: "food-diary-built-with-ai.firebaseapp.com",
+  projectId: "food-diary-built-with-ai",
+  storageBucket: "food-diary-built-with-ai.firebasestorage.app",
+  messagingSenderId: "153168289620",
+  appId: "1:153168289620:web:b00cea462bd16069e1f2c1"
+};
